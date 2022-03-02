32327920dbcae5f1c773f3b5321bda7ef0c10f08
Grzesiek Zatorski <g.zatorski90@wp.pl>
2022-03-02 13:34:19 +0100

    Dodaj pliki projektów.

---
+++ Catch_the_Panda/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Catch_the_Panda.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Catch_the_Panda.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Catch_the_Panda/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Catch_the_Panda.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
